db351177000e49e255866236b85786968391ffc6
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-07-05 22:15:04 -0700

    update2

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 // Assignment code here
+const form = document.getElementById("stopper")
 
+const UpperCase_char = arryFromLowToHigh(65, 90)
+const LowerCase_char = arryFromLowToHigh(97, 122)
+const Numbers_char = arryFromLowToHigh(48, 57)
+const Symbols_char = arryFromLowToHigh(33, 47).concat(
+  arryFromLowToHigh(58, 64)
+)
 
-function generatePassword() {
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const password = generatePassword(characterAmount, IncludeUppercase, includeNumbers, IncludeSymbols)
+})
+
+function generatePassword(characterAmount, includeNumbers, IncludeUppercase, IncludeSymbols) {
   prompt("how ong is your password?")
-  return ("") 
+  String.fromCharCode
+}
+
+function arryFromLowToHigh(Low, High){
+  const array = []
+  for (let i = low; i <= High; I++) {
+   array.push(i)
+  }
+  return array
 }
 
 // Get references to the #generate element
